78ed4b440e61a08899b8f3bd79432ca1ab121456
darknessdocetw <sky712933@gmail.com>
2014-10-25 04:45:47 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BoardTestV1O::Application.config.secret_key_base = '38369228329df7b0680e3916a7bd869a6c9896951d30c1d9b7dca87c2014eb3b793700f87d3b7fbbb0206f5142f3ab92320a89faa7a68b77de12952b866d94d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BoardTestV1O::Application.config.session_store :cookie_store, key: '_Board_test_v1_o_session'
